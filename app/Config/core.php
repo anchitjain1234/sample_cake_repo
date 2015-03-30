4d1428ff1131d4970c00eae66c685a3307d80b87
Anchit Jain <anchitjain1234@gmail.com>
2015-03-30 18:26:38 +0530

    cipher seed changed to numeral only

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', '1234567890werwer');
+	Configure::write('Security.salt', '123456789012213123123werwer');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', 'wrwetdfgdfgdfg565434534534');
+	Configure::write('Security.cipherSeed', '1233123123123123123123');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
